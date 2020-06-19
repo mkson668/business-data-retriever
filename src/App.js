ef0e29105f040aaa69b60396dbc95d44a89e4a71
aaron <aaronwong668@gmail.com>
2020-06-19 01:43:42 -0700

    update custom favicon and check functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          React JSSSSSSSSSSSS. reactDOM
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
