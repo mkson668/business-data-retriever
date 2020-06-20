13a68a1e7841b7730f1b770448cf55d992b41513
aaron <aaronwong668@gmail.com>
2020-06-20 02:23:06 -0700

    changed img css for businesses, photoshopped new blurred background for text visibility and modified business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -19,8 +19,8 @@ class Business extends React.Component {
     render() {
         return (
             <div className="Business">
-                <div className="image-container">
-                    <img src={businessPlaceholder.imageSrc} alt='' />
+                <div className="image-wrapper">
+                    <img className="image-in-wrapper" src={businessPlaceholder.imageSrc} alt='' />
                 </div>
                 <h2>{businessPlaceholder.name}</h2>
                 <div className="Business-information">
@@ -31,8 +31,8 @@ class Business extends React.Component {
                     </div>
                     <div className="Business-reviews">
                         <h3>{businessPlaceholder.category}</h3>
-                        <h3 className="rating">{businessPlaceholder.rating + 'stars'}</h3>
-                        <p>{businessPlaceholder.reviewCount + 'reviews'}</p>
+                        <h3 className="rating">{businessPlaceholder.rating + ' stars'}</h3>
+                        <p>{businessPlaceholder.reviewCount + ' reviews'}</p>
                     </div>
                 </div>
             </div>
