38bbf778a33dc1493c29126f496064d05830bd5f
aaron <aaronwong668@gmail.com>
2020-06-22 15:50:06 -0700

    slight adjustments to layout

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -19,19 +19,19 @@ class Business extends React.Component {
         return (
             <div className="Business">
                 <div className="image-wrapper">
-                    <img className="image-in-wrapper" src={this.props.imageSrc} alt='' />
+                    <img className="image-in-wrapper" src={this.props.businessData.imageSrc} alt='' />
                 </div>
-                <h2>{this.props.name}</h2>
+                <h2>{this.props.businessData.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{this.props.address}</p>
-                        <p>{this.props.city}</p>
-                        <p>{this.props.state}</p>
+                        <p>{this.props.businessData.address}</p>
+                        <p>{this.props.businessData.city}</p>
+                        <p>{this.props.businessData.state}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{this.props.category}</h3>
-                        <h3 className="rating">{this.props.rating + ' stars'}</h3>
-                        <p>{this.props.reviewCount + ' reviews'}</p>
+                        <h3>{this.props.businessData.category}</h3>
+                        <h3 className="rating">{this.props.businessData.rating + ' stars'}</h3>
+                        <p>{this.props.businessData.reviewCount + ' reviews'}</p>
                     </div>
                 </div>
             </div>
